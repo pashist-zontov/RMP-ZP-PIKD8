2f98d6534e06d61ae2ff8c0fc51f1467f8543a8c
Pavel Zorin <sistpavel@gmail.com>
2025-05-01 21:19:57 +1000

    PZ1 DONE

---
+++ app/src/main/java/ru/fefu/helloworld/HelloWorld.kt
@@ -0,0 +1,8 @@
+package ru.fefu.helloworld
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HelloWorld : AppCompatActivity(R.layout.activity_helloworld){
+}
